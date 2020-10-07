693e109ceef18e02d841f31d79e52cb3e85c6504
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-10-08 05:45:05 +0800

    Update index.vue

--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -1,89 +1,31 @@
 <template>
-<div>
-    <div class="nchu main">
-        <Form
-            :form-mode="'schedule'"
-        ></Form>
-        <div class="ts static left sidebar">
-            <div class="index">NCHU</div>
-            <div class="ts divider"></div>
-            <div class="ts divider"></div>
-            <div class="ts divider"></div>
-            <div class="ts divider"></div>
-            <div class="ts divider"></div>
-        </div>
-        <div class="squeezable pusher">
-            <header>
-                <div class="ts pointing secondary large icon menu">
-                    <a
-                        class="item"
-                        style="z-index: 2;"
-                        id="menu-button"
-                        data-tooltip="選單"
-                        @click="toggleSidebar()"
-                    ><i class="list icon"></i></a>
-                    <a
-                        class="item nchu header title"
-                        style="z-index: 2;"
-                        data-tooltip="首頁"
-                    >中興大學場地借用系統</a>
-                    <div class="right menu">
-                        <!-- <div class="item">OOO，您好</div> -->
-                        <a
-                            href=""
-                            class="item"
-                            style="z-index: 2;"
-                            data-tooltip="意見反饋"
-                        ><i class="mail outline icon"></i></a>
-                        <div class="item">登入｜註冊</div>
-                        <!-- <a
-                            href=""
-                            class="item"
-                            style="z-index: 2;"
-                            data-tooltip="個人檔案"
-                        ><i class="user outline icon"></i></a>
-                        <a
-                            href=""
-                            class="item"
-                            style="z-index: 2;"
-                            data-tooltip="登出"
-                        ><i class="log out icon"></i></a> -->
-                    </div>
-                </div>
-            </header>
-            <Calendar></Calendar>
-        </div>
-    </div>
-</div>
+<MainLayout
+    :has-form="true"
+>
+    <Calendar
+        slot="content"
+    ></Calendar>
+</MainLayout>
 </template>
 
 <script>
+import MainLayout from '../layouts/main.vue'
 import Calendar from '../components/calendar/calendar.vue'
-import Form from '../layouts/form.vue'
 
 export default {
     data() {
         return {
-            user: {
-                id: null,
-                name: null,
-                util: null,
-                mail: null,
-                phone: null,
-            }
         };
     },
     mounted() {
         console.log(this);
     },
     components: {
+        MainLayout,
         Calendar,
-        Form
     },
     methods: {
-        toggleSidebar() {
-            ts('.left.sidebar').sidebar('toggle');
-        },
+
     }
 }
 </script>
