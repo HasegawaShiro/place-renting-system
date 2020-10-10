333a934c4be90fed38c4dc1581119fc0b0852201
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-10-11 04:09:44 +0800

    Update constants.js

--- resources/nuxt/constants.js
+++ resources/nuxt/constants.js
@@ -4,7 +4,7 @@ export default {
             menu: '選單',
             index: '首頁',
             opinion: '意見反饋',
-            title: '中興大學場地借用系統',
+            title: '中興大學創產學院場地借用系統',
             login: '登入',
             register: '註冊',
             greet: '您好',
@@ -14,7 +14,15 @@ export default {
             password: '密碼',
         },
     },
-    schedule:{},
+    schedule:{
+        TEXT: {
+            selected_date: '當前所選日期',
+            today_event: '本日活動',
+            year: '年',
+            month: '月',
+            day: '日',
+        },
+    },
     calendar:{
         MODE_BUTTON: {
             'month': '月表',
@@ -22,9 +30,14 @@ export default {
             'list': '列表'
         },
         HEADER_TEXT: {
+            title: '場地使用查詢',
             date: '日期',
             place: '場地'
         },
         DAY_TEXT: ['一','二','三','四','五','六','日'],
+    },
+    messages: {
+        'login-success': '登入成功',
+        'user-disabled': '此用戶已遭停用',
     },
 }
