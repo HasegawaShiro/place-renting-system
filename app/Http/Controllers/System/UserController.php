097167a0cca8a9c54b4d0cec5ef48abe9cf09801
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-10-13 01:01:59 +0800

    Create UserController::getUserSession

---
+++ app/Http/Controllers/System/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\System;
+
+use Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller {
+    public function getUserSession() {
+        $user = Request::session()->get('user');
+
+        return response()->json(['user' => $user]);
+    }
+
+}
