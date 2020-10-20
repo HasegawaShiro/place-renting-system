242d68f19f303f0e9b8a60f2ffefc435c1be3607
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-10-21 05:59:32 +0800

    Finish PageUtil::validationForSave

--- app/Utils/PageUtil.php
+++ app/Utils/PageUtil.php
@@ -2,9 +2,31 @@
 namespace App\Utils;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class PageUtil {
-    public static function validateForSave(String $table, Array $data, String $status) {
-        return true;
+    public static function validateForSave(String $table, Array &$data, String $status, Array &$result) {
+        $class = "App\\Pages\\".ucfirst($table);
+        $pass = true;
+        if(class_exists($class)){
+            $page = new $class();
+            $validator = Validator::make($data, $page::fields()[$status], [], $page::fields()['attributes']);
+
+            // return $validator;
+            if($validator->fails()){
+                foreach($validator->errors()->toArray() as $error){
+                    foreach($error as $message){
+                        array_push($result['messages'], $message);
+                    }
+                }
+                $pass = false;
+            }else{
+                if(method_exists($page, 'afterValidation')){
+                    $pass = $page::afterValidation($data, $result, $status);
+                }
+            }
+        }
+
+        return $pass;
     }
 }
