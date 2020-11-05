5dd43f4d6dd36ad9dbe4607319e49007f11cda8c
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-11-05 10:03:50 +0800

    Add $fail & $error in App\Models\Model

--- app/Models/Concerns/CustomAttributes.php
+++ app/Models/Concerns/CustomAttributes.php
@@ -12,4 +12,12 @@ trait CustomAttributes
     public function isEditable(String $attribute) {
         return empty($this->editable) || array_search($attribute, $this->editable) !== false;
     }
+
+    protected $fail = false;
+    public function fails() {$this->fail = true;}
+    public function isFailed() {return $this->fail;}
+
+    protected $error = null;
+    public function setError($data) {$this->error = $data;}
+    public function getError() {return $this->error;}
 }
