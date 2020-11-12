fb96300807d90ea189bf2d2cb25e897297d0f73a
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-11-12 18:04:13 +0800

    Update PageUtil

--- resources/nuxt/utils/PageUtil.js
+++ resources/nuxt/utils/PageUtil.js
@@ -6,24 +6,118 @@ export default class PageUtil {
         const classes = {
             user: User,
             schedule: Schedule,
+            announcement: Announcement,
+            util: Util,
+            place: Place,
+            opinion: Opinion,
         }
 
         page = page.toLowerCase();
         if(!DataUtil.isEmpty(classes[page])){
-            return classes[page];
+            return new classes[page]();
         }else{
             console.error("The page is not found.");
         }
     }
 }
 
-class User {}
+class User {
+    #Name = "user"
+    #Title = "用戶資料"
+    fields() {
+        return [
+            (new Field(
+                'name',
+                '姓名',
+                'text',
+                {
+                    listOrder: 0,
+                    formOrder: 0,
+                }
+            )),
+            (new Field(
+                'phone',
+                '電話',
+                'text',
+                {
+                    listOrder: 1,
+                    formOrder: 1,
+                }
+            )),
+            (new Field(
+                'email',
+                '電子信箱',
+                'text',
+                {
+                    listOrder: 2,
+                    formOrder: 2,
+                }
+            )),
+            (new Field(
+                'util_id',
+                '所屬單位',
+                'select',
+                {
+                    selectOptions: 'util',
+                    showOnList: false,
+                    formOrder: 3,
+                }
+            )),
+            (new Field(
+                'util_name',
+                '所屬單位',
+                'text',
+                {
+                    listOrder: 3,
+                    showOnForm: false,
+                }
+            )),
+            (new Field(
+                'username',
+                '帳號',
+                'text',
+                {
+                    editable: false,
+                    listOrder: 4,
+                    formOrder: 4,
+                }
+            )),
+            (new Field(
+                'password',
+                '密碼',
+                'password',
+                {
+                    showOnList: false,
+                    formOrder: 5,
+                    confirmation: true,
+                    required: false,
+                    addRequired: true,
+                    hideOnView: true,
+                }
+            )),
+            (new Field(
+                'user_disabled',
+                '停用',
+                'boolean',
+                {
+                    trueText: '是',
+                    falseText: '否',
+                    listOrder: 5,
+                    formOrder: 6,
+                    default: false,
+                    required: false,
+                }
+            )),
+        ]
+    }
+
+    get Name() {return this.#Name}
+    get Title() {return this.#Title}
+}
 
 class Schedule {
-    static fields() {
-        /* let places = [];
-        API.getReferenceSelect("place").then(response => places = response).catch(e => {}); */
-
+    #Name = "schedule"
+    fields() {
         return [
             (new Field(
                 'schedule_date',
@@ -161,33 +255,206 @@ class Schedule {
             )), */
         ];
     }
-    static text() { return '行程' }
-    static listData() {
-        return {};
-    }
+
+    get Name() {return this.#Name}
 }
 
-class Form {
-    #Name;
-    #Text;
-    #Fields = [];
-    static classes() {
-        return {
-            'user': User,
-            'schedule' : Schedule,
-        }
-    };
-
-    constructor(name){
-        if(Form.classes[name]){
-            const page = Form.classes[name];
-            this.#Name = name;
-            this.#Text = page.text();
-            this.#Fields = page.fields();
-        }else{
-            console.error('Page not found.');
-        }
+class Announcement {
+    #Name = "annoucement"
+    #Title = "公告"
+    fields() {
+        return [
+            (new Field(
+                    'announcement_title',
+                    '標題',
+                    'text',
+                    {
+                        listOrder: 2,
+                        formOrder: 0,
+                    }
+              )),
+              (new Field(
+                    'announcement_type',
+                    '公告類型',
+                    'select',
+                    {
+                        selectOptions: {
+                            announcement: '公告',
+                            update: '系統更新',
+                        },
+                        listOrder: 1,
+                        formOrder: 1,
+                        default: 'announcement',
+                    }
+              )),
+              (new Field(
+                    'announcement_date',
+                    '發佈日期',
+                    'date',
+                    {
+                        listOrder: 0,
+                        formOrder: 2,
+                    }
+              )),
+              (new Field(
+                    'announcement_content',
+                    '內容',
+                    'textarea',
+                    {
+                        showOnList: false,
+                        formOrder: 3,
+                        required: false,
+                    }
+              )),
+              (new Field(
+                    'util_id',
+                    '承辦單位',
+                    'select',
+                    {
+                        selectOptions: 'util',
+                        listOrder: 4,
+                        formOrder: 4,
+                    }
+              )),
+              (new Field(
+                    'user_id',
+                    '承辦人',
+                    'select',
+                    {
+                        selectOptions: 'user',
+                        filter: {
+                            foreignField: 'util_id',
+                            target: 'util_id',
+                            from: 'util'
+                        },
+                        listOrder: 5,
+                        formOrder: 5,
+                    }
+              )),
+              (new Field(
+                    'announcement_file',
+                    '附件下載',
+                    'file',
+                    {
+                        listOrder: 3,
+                        formOrder: 6,
+                        required: false,
+                    }
+              )),
+        ];
     }
+
+    get Name() {return this.#Name}
+    get Title() {return this.#Title}
+}
+
+class Util {
+    #Name = "util"
+    #Title = "所屬單位資料"
+    fields() {
+        return [
+            (new Field(
+                'util_code',
+                '所屬單位代碼',
+                'text',
+                {
+                    editable: false
+                }
+            )),
+            (new Field(
+                'util_name',
+                '所屬單位名稱',
+                'text',
+                {
+                    listOrder: 1,
+                    formOrder: 1,
+                }
+            )),
+            (new Field(
+                'remarks',
+                '備註',
+                'text',
+                {
+                    listOrder: 2,
+                    formOrder: 2,
+                    required: false,
+                }
+            )),
+            (new Field(
+                'util_disabled',
+                '停用',
+                'boolean',
+                {
+                    trueText: '是',
+                    falseText: '否',
+                    listOrder: 3,
+                    formOrder: 3,
+                    required: false,
+                    default: false,
+                }
+            )),
+        ];
+    }
+    get Name() {return this.#Name}
+    get Title() {return this.#Title}
+}
+
+class Place {
+    #Name = "place"
+    #Title = "場地基本資料"
+    fields() {
+        return [
+            (new Field(
+                'place_code',
+                '場地代碼',
+                'text',
+                {
+                    editable: false,
+                }
+            )),
+            (new Field(
+                'place_name',
+                '場地名稱',
+                'text',
+                {
+                    listOrder: 1,
+                    formOrder: 1,
+                }
+            )),
+            (new Field(
+                'remarks',
+                '備註',
+                'text',
+                {
+                    listOrder: 2,
+                    formOrder: 2,
+                    required: false,
+                }
+            )),
+            (new Field(
+                'place_disabled',
+                '停用',
+                'boolean',
+                {
+                    trueText: '是',
+                    falseText: '否',
+                    listOrder: 3,
+                    formOrder: 3,
+                    default: false,
+                }
+            )),
+        ];
+    }
+
+    get Name() {return this.#Name}
+    get Title() {return this.#Title}
+}
+
+class Opinion {
+    #Name = "user"
+    #Title = "用戶資料"
+    get Name() {return this.#Name}
+    get Title() {return this.#Title}
 }
 
 class Field {
@@ -199,7 +466,9 @@ class Field {
         editable: true,     // 新增可以輸入，若為false則修改不能動
         showOnList: true,   // 是否出現在列表畫面
         showOnForm: true,   // 是否出現在表單畫面
+        hideOnView: false,
         selectOptions: [],  // 下拉選項：如果是動態下拉，就直接寫table名稱'user', 若為固定下拉，則寫Array: [1,2, '字串']
+        filter: {},
         remarks: null,      // 顯示於欄位下方的備註文字
         trueText: null,     // boolean型態，出現在列表時，若為True，則顯示的文字
         falseText: null,    // boolean型態，出現在列表時，若為False，則顯示的文字
@@ -208,8 +477,12 @@ class Field {
         listOrder: 0,       // 列表欄位排序
         formOrder: 0,       // 表單欄位排序
         required: true,     // 是否必填
+        addRequired: false,
+        editRequired: false,
+        default: null,
+        confirmation: false,
     };
-    static allowType = ['text', 'textarea', 'number', 'boolean', 'date', 'time', 'datetime', 'select', 'custom'];
+    static allowType = ['text', 'password', 'textarea', 'number', 'boolean', 'date', 'time', 'datetime', 'select', 'file', 'custom'];
 
     constructor(name, text = null, type = 'string', options = {}) {
         this.#Name = name;
