28e5faa8400fa0e89c7d431c3decc8b1eddef8c9
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-11-12 17:58:13 +0800

    Update seeder

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -15,11 +15,11 @@ class TestDataSeeder extends Seeder
     public function run()
     {
         Place::insert([
-            ['place_name' => '測試場地1', 'place_disabled' => false],
-            ['place_name' => '測試場地2', 'place_disabled' => false],
-            ['place_name' => '測試場地3', 'place_disabled' => false],
-            ['place_name' => '測試場地4', 'place_disabled' => false],
-            ['place_name' => '測試場地5', 'place_disabled' => true],
+            ['place_code' => 'a', 'place_name' => '測試場地1', 'place_disabled' => false],
+            ['place_code' => 'b', 'place_name' => '測試場地2', 'place_disabled' => false],
+            ['place_code' => 'c', 'place_name' => '測試場地3', 'place_disabled' => false],
+            ['place_code' => 'd', 'place_name' => '測試場地4', 'place_disabled' => false],
+            ['place_code' => 'e', 'place_name' => '測試場地5', 'place_disabled' => true],
         ]);
     }
 }
