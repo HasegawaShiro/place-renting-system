b60db6b9e93f367947e52fc8397aa8dd3276dc5a
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-12-15 10:14:47 +0800

    Finish error/403 auto redirect

--- resources/nuxt/pages/errors/403.vue
+++ resources/nuxt/pages/errors/403.vue
@@ -6,20 +6,38 @@
         class="ts inverted negative segment"
         slot="content"
     >
-        您沒有權限探訪此頁面，請確認是否已登入，或登入之用戶組是否正確。
+        您沒有權限探訪此頁面，請確認是否已登入，或登入之用戶組是否正確。<br>
+        將於 {{second}} 秒後跳轉回首頁，或<a id="to-index" @click="redirectToIndex">點擊此直接跳轉</a>
     </div>
 </MainLayout>
 </template>
 
 <script>
 import MainLayout from '../../layouts/main.vue';
+import Funtions from '../../functions.js';
 
 export default {
-    mounted() {
+    data() {
+        return {
+            second: 5,
+        }
+    },
+    async mounted() {
         window.mainLayout.contentLoaded();
+        while (this.second > 0) {
+            await Funtions.delay(1000);
+            this.second--;
+        }
+        this.redirectToIndex();
     },
     components: {
         MainLayout,
+    },
+    methods: {
+        redirectToIndex() {
+            this.$router.push('/');
+            // return redirect('/')
+        },
     },
 }
 </script>
@@ -28,5 +46,18 @@ export default {
     .ts.inverted.negative.segment {
         margin: 2em;
         border-left: solid .35em rgb(146, 52, 52);
+    }
+    #to-index {
+        color: white;
+    }
+    @media(hover: hover) and (pointer: fine) {
+        #to-index:hover {
+            color: rgb(250, 250, 250);
+            cursor: pointer;
+            text-decoration: underline;
+        }
+    }
+    #to-index:active {
+        color: rgb(212, 212, 212);
     }
 </style>
