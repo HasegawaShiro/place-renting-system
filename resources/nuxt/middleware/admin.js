0213561938301faa740d610f276cd77c264a06b6
Hasegawa_Shiro <shiraha.soujin@gmail.com>
2020-12-17 12:45:06 +0800

    Fix middleware/admin

--- resources/nuxt/middleware/admin.js
+++ resources/nuxt/middleware/admin.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-
 export default async function({
     redirect
 }) {
     let isAdmin = true;
     // console.log('middleware: admin')
-    await axios.get('/api/auth').then(response => {
+    await window.$nuxt.$axios.get('/api/auth').then(response => {
         let user = response.data.user;
         if(user.id !== 1) isAdmin = false;
     }).catch(e => {
